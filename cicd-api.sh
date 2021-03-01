37523251ad2178b53d0273fdac38411b21942a04
shaikmohammed12 <59485191+shaikmohammed12@users.noreply.github.com>
2021-03-01 12:47:33 +0530

    Update cicd-api.sh

--- cicd-api.sh
+++ cicd-api.sh
@@ -1,3 +1,5 @@
+curl -X POST -H "Content-type: application/json" -H "Accept:application/json" -H "Authorization: Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJmZjZjZWQxOS03MTFlLTQ1OGEtOTJjZS1kNDQ1MjgwZTM0YTIiLCJkb21haW4iOiJzYWtoYXRlY2guY29tIn0.57hAcxtgA6ez_Ww6OT43NXub9wWXVLPNu9HozCde8p1kOmqE5MoLUcTSZMf7nHZ1NB0BUyX-klzmOLiADkpExQ" https://app.testsigma.com/api/v1/execution_results -d "{\"executionId\": \"151\"}"
+
 TESTSIGMA_API_KEY= eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJmZjZjZWQxOS03MTFlLTQ1OGEtOTJjZS1kNDQ1MjgwZTM0YTIiLCJkb21haW4iOiJzYWtoYXRlY2guY29tIn0.57hAcxtgA6ez_Ww6OT43NXub9wWXVLPNu9HozCde8p1kOmqE5MoLUcTSZMf7nHZ1NB0BUyX-klzmOLiADkpExQ
 
 TESTSIGMA_TEST_PLAN_ID= 151
